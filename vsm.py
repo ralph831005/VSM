88ce4515de76f573e859145813d2f038ebe5c93c
RalphLee <ralph831005@gmail.com>
2016-03-29 19:38:33 +0800

    sparse matrix

--- vsm.py
+++ vsm.py
@@ -2,41 +2,45 @@
 import numpy as np
 import os
 import math
+from collections import defaultdict
 invert_file_path = '/tmp3/ralph831005/IR/model/inverted-file'
 total_doc = 46972
-
+total_grams = 1193467
 class VSM:
     def __init__(self):
-        self.tf_idf = [[] for i in range(total_doc)]
-        self.v_length = [0 for i in range(total_doc)]
-        self.index = []
-    def weight(self, file_path=invert_file_path, tf_func=lambda tf, mtf: 0.5 + 0.5 * (float(tf)/float(mtf))):
+        self.tf_idf = [dict() for i in range(total_doc)]
+        self.v_length = np.zeros(total_doc)
+        self.idf = np.zeros(total_grams)
+        self.index = defaultdict(dict)
+    def weight(self, file_path=invert_file_path, tf_func=lambda tf, mtf: 0.5 * (float(tf)/float(mtf))):
         with open(file_path, 'r') as inverted_file:
+            counter = 0
             while True:
                 line = inverted_file.readline()
                 if len(line) == 0:
                     break
                 bigram = line.strip().split()
-                self.index.append(bigram[0:2])
-                idf = math.log(total_doc/float(bigram[2]))
-                lines = dict([tuple([int(x) for x in inverted_file.readline().strip().split()]) for i in range(int(bigram[2]))])
-                max_tf = max(lines.values())
-                for i in range(total_doc):
-                    if i in lines:
-                        self.tf_idf[i].append(tf_func(lines[i], max_tf) * idf)
-                        self.v_length += self.tf_idf[i][-1] ** 2
-                    else:
-                        self.tf_idf[i].append(tf_func(0, max_tf) * idf)
-                        self.v_length += self.tf_idf[i][-1] ** 2
-                        
+                self.index[int(bigram[0])][int(bigram[1])] = counter
+                self.idf[counter] = math.log(total_doc/float(bigram[2]))
+                lines = [[int(x) for x in inverted_file.readline().strip().split()] for i in range(int(bigram[2]))]
+                max_tf = max(list(zip(*lines))[1])
+                for doc in lines:
+                    self.tf_idf[doc[0]][counter] = tf_func(doc[1], max_tf) * self.idf[counter]
+                    self.v_length[doc[0]] += self.tf_idf[doc[0]][counter] ** 2
+                counter += 1
+        print('weighting done')
+        
     def consine_similarity(self, query):
-        similarity = []
+        similarity = np.zeros(total_doc)
         for i, doc in enumerate(self.tf_idf):
-            similarity.append(np.dot(doc, query.vector) / (query.v_length ** (0.5)) / (self.v_length[i] ** (0.5)))
+            for k, v in query.sparse.items():
+                if k in doc:
+                    similarity[i] += doc[k]*v
+            similarity[i] = similarity[i]*(self.v_length[i]**(0.5))
+        return sort(list(zip(range(total_doc), similarity)))
 
 if __name__ == '__main__':
     vsm = VSM()
-    vsm.weight()
+    vsm.weight(tf_func = lambda tf, mtf: float(tf)/float(mtf))
     print(vsm.tf_idf[33689])
-    print(len(vsm.tf_idf[0]))
 
